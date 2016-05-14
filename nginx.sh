bf86fd1a07baa425a38e3930f74b13c56a35f4f2
whxmail <syseye@163.com>
2016-05-14 15:41:31 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --name whx-nginx --link whx-mysql:mysql -d -P -p 80:80 -v $PWD/whxmail.com:/var/www/whxmail.com whx/nginx
+docker run --name whx-nginx --link whx-mysql:mysql -d -P -p 80:80 -v $PWD/../whx/whxmail.com:/var/www/whxmail.com whx/nginx
 
